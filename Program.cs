553f3100e06ceaee15d5bb7e41a1d61749144a34
Rauf84 <rauf@karimli.se>
2020-12-03 10:36:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Inlamningsuppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till informationsportalen om basgrupper!");
+            Inlogg();
+            //Menu();
+        }
+
+        static void Inlogg()
+        {
+            string lösenord = "";
+
+            while (lösenord != "coffencode")
+            {
+                Console.Write("Vänligen ange kod: ");
+                lösenord = Console.ReadLine();
+                if (lösenord == "q")
+                {
+                    Console.WriteLine("Tack och välkommen åter!");
+                    break;
+                }
+                
+                if (lösenord == "coffencode")
+                {
+                    Console.WriteLine("Du är inloggad!");
+                }
+                else
+                {
+                    Console.WriteLine("Fel kod, vänligen försök igen eller ange q + Enter för att avsluta");
+
+                }
+
+            }
+        }
+
+       /* static void Menu()
+        {
+            enum menu
+             {
+                 coffencodeGroup = 1,
+                 memberDetails = 2,
+             }
+        }*/
+    }
+
+}
